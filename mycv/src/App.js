b165bff438bfbc31997cfed03c2b10ac6c877f68
IhorKurylov <idSNrY7I3xhB>
2023-03-01 20:44:48 +0200

    init group project

--- mycv/src/App.js
+++ mycv/src/App.js
@@ -3,17 +3,7 @@ import logo from './logo.svg';
 function App() {
   return (
     <div>
-      <h1> Ihor changes</h1>
-      <h1> Ihor changes</h1>
-      <h1> Ihor changes</h1>
-      <h1> Ihor changes</h1>
-      <h1> Ihor changes</h1>
-      <h1> Ihor changes</h1>
-      <h1> Ihor changes</h1>
-        <button>Mariia Btn</button>
-        <button>Mariia Btn</button>
-        <button>Mariia Btn</button>
-        <button>Mariia Btn</button>
+
     </div>
   );
 }
