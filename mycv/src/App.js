eacdb81d6d77d2166341b910a14166cc7f71b5ea
oksmelnyk <bellezzaok@gmail.com>
2023-03-02 16:24:38 +0200

    Added OksanaComponent

--- mycv/src/App.js
+++ mycv/src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
+import OksanaComponent from './components/OksanaComponent';
 
 function App() {
   return (
     <div>
-
+      <OksanaComponent/>
     </div>
   );
 }
