898d8a8451c959857d16e90201cfbd36805130f5
oksmelnyk <bellezzaok@gmail.com>
2023-03-08 18:26:16 +0200

    created public and private routes

--- mycv/src/common/router.js
+++ mycv/src/common/router.js
@@ -2,7 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./AppRoutes";
 import OksanaComponent from "../components/Oksana/OksanaComponent";
 import App from "../App";
-import RouteComponent from '../components/Oksana/RouteComponent';
+
+import RouteComponent, {
+    CheckAccess,
+    RouteAdminComponent,
+    RouteLoginComponent,
+    RouteUserComponent
+} from '../components/Oksana/RouteComponent';
 
 export const router = createBrowserRouter([
     {
@@ -19,6 +25,26 @@ export const router = createBrowserRouter([
                 path: AppRoutes.ROUTE_OKS,
                 element: <RouteComponent/>,
                 errorElement: <h1>NOT FOUND</h1>,
+                children: [
+                    {
+                        path: AppRoutes.ROUTE_OKS_LOGIN,
+                        element: <RouteLoginComponent/>,
+                    },
+                    {
+                        path: AppRoutes.ROUTE_OKS_USER,
+                        element: <CheckAccess
+                          permissions={['user', 'admin']}
+                          Component={RouteUserComponent}
+                        />,
+                    },
+                    {
+                        path: AppRoutes.ROUTE_OKS_ADMIN,
+                        element: <CheckAccess
+                          permissions={['admin']}
+                          Component={RouteAdminComponent}
+                        />,
+                    },
+                ]
             },
             {
                 path: AppRoutes.IHOR,
--- mycv/src/components/Oksana/RouteComponent.js
+++ mycv/src/components/Oksana/RouteComponent.js
@@ -1,9 +1,72 @@
+import { Link, Outlet, useNavigate } from 'react-router-dom';
 import style from './RouteComponent.module.scss'
+import { AppRoutes } from '../../common/AppRoutes';
+
+export const RouteLoginComponent = () => {
+  const navigate = useNavigate()
+  const remember = (type) => localStorage.setItem('permission', type)
+  const logout = () => {
+    localStorage.removeItem('permission')
+    navigate(AppRoutes.ROUTE_OKS)
+  }
+  return (
+    <div>
+      <button onClick={() => remember('user')}>
+        Made User
+      </button>
+      <button onClick={() => remember('admin')}>
+        Made Admin
+      </button>
+      <button onClick={() => logout()}>
+        Logout
+      </button>
+    </div>
+  )
+}
+
+export const RouteUserComponent = () => {
+  return (
+    <div>
+      USER PAGE
+    </div>
+  )
+}
+
+export const RouteAdminComponent = () => {
+  return (
+    <div>
+      ADMIN PAGE
+    </div>
+  )
+}
+
+export const CheckAccess = ({permissions, Component }) => {
+  const currentPermission = localStorage.getItem('permission')
+  return currentPermission && permissions.includes(currentPermission) ? <Component/> : (
+    <div>
+      ACCESS DENIED!!! Your permission is <strong>{currentPermission ?? <span>N/A</span>}</strong>
+    </div>
+  )
+}
+
 
 const RouteComponent = () => {
   return (
     <div className={style.container}>
-      <img src="https://i0.wp.com/www.kulemet.com/wp-content/uploads/2019/08/taksi.jpg?fit=632%2C474&ssl=1"/>
+      <div className={style.navigation}>
+        <Link to={AppRoutes.ROUTE_OKS_LOGIN}>
+          Login
+        </Link>
+        <Link to={AppRoutes.ROUTE_OKS_USER}>
+          User
+        </Link>
+        <Link to={AppRoutes.ROUTE_OKS_ADMIN}>
+          Admin
+        </Link>
+      </div>
+      <div className={style.content}>
+        <Outlet/>
+      </div>
     </div>
   )
 }
