722f5b5f75b338f252298884647f3a3f8e99598c
IhorKurylov <idSNrY7I3xhB>
2023-04-05 19:35:49 +0300

    added links

--- mycv/src/Views/Mentor/components/FIREBASE/FirebaseComponent.jsx
+++ mycv/src/Views/Mentor/components/FIREBASE/FirebaseComponent.jsx
@@ -1,10 +1,13 @@
-import { Outlet} from "react-router-dom";
+import {Link, Outlet} from "react-router-dom";
+import {AppRoutes} from "../../../../common/AppRoutes";
 
 const FirebaseComponent = () => {
-
+    const authUser = JSON.parse(localStorage.getItem('authUser'))
     return(
         <div>
             <h1>FirebaseComponent</h1>
+            <Link to={AppRoutes.MENTOR_LOGIN}>login</Link>
+            <Link to={`/users/${authUser.uid}`}>userPage</Link>
             <Outlet />
         </div>
     )
